c8ea92a5edbe71eb98bd58b7661b689e87d8d525
Appologetic <spencer.fietz@gmail.com>
2023-09-10 18:44:59 -0600

    Express Set Up and Route Handling Done

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-let baseURL = "https://dueordie.com"
-let urlExtension = "/sign-up"
-let mockURL = baseURL+urlExtension
+// ----- External Dependencies -----
+const express = require("express");
 
-console.log(mockURL)
+// ----- Instantiate Express App ------
+const app = express();
 
-if (mockURL == "https://dueordie.com") {
-    console.log("Welcome to the homepage of the website")
-}
-else if(mockURL == "https://dueordie.com/sign-up"){
-    console.log("This is the sign up page")
-}
-else{
-    console.log("404: that URL does not exist")
-}
+// ----- Route Handling -----
+app.get("/", (req, res) =>{
+    console.log(req.method, " request received at ",req.url)
+    res.send("Welcome to Due or Die")
+})
+//if (mockURL == "https://dueordie.com") {
+//    console.log("Welcome to the homepage of the website")
+//}
+
+app.get("/sign-up", (req, res) =>{
+    console.log(req.method, " request received at ",req.url)
+    res.send("This is the sign up page")
+})
+//else if(mockURL == "https://dueordie.com/sign-up"){
+//    console.log("This is the sign up page")
+//}
+
+app.use((req, res) =>{
+    res.status(404).send("404: That URL does not exist")
+})
+//else{
+//    console.log("404: that URL does not exist")
+//}
+
+
+// ----- Start Express App -----
+app.listen(5050, () => console.log("Listening for requests on port 5050"))
